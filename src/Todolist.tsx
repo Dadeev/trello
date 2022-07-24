74a31419b6fb6433427126b564ea10505ed1d70f
Tima <dadeev.tim@gmail.com>
2022-07-24 22:38:40 +0700

    refactor: correct some plases of the project

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -58,7 +58,7 @@ export function Todolist(props: PropsType) {
         <div>
             <input value={title}
                    onChange={onChangeHandler}
-                   onKeyPress={onKeyPressHandler}
+                   onKeyDown={onKeyPressHandler}
                    className={error ? "error" : ""}
             />
             <button onClick={addTask}>+</button>
