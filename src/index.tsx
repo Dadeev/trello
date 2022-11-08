2ffee1f95d69dddd47e776091269cbd1313b2536
Tima <dadeev.tim@gmail.com>
2022-11-08 17:19:37 +0700

    fix: correct some routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
